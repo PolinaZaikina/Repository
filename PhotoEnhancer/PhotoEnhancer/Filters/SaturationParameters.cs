1840eaee9cf22f92ad4a30dc47ec216a409658d9
Polina.Zaikina <Polina.Zaikina@urfu.me>
2023-06-11 23:24:45 +0500

    ShiftParameters ShiftTransformer

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/SaturationParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class SaturationParameters : IParameters
+    {
+        [ParameterInfo(Name = "Коэффициент",
+            MinValue = 0,
+            MaxValue = 10,
+            DefaultValue = 1,
+            Increment = 0.05)]
+        public double Coefficient { get; set; }
+
+    }
+    
+}
+
+
